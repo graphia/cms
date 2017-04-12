184274127f8e85be56de232cb309cb703a16ec2a
Peter Yates <peter.yates@graphia.co.uk>
2017-04-12 19:24:18 +0100

    Add a very rudimentary flexbox and css-grid layout
    A quick first attempt at a sensible layout. The vertical sections of the
    page are in a flex column[0], and the navigation sections, primary
    horizontally under the header and secondary in the left column, and the
    application view are in a css-grid[1].
    
    [0] https://developer.mozilla.org/en/docs/Web/CSS/flex
    [1] https://developer.mozilla.org/en/docs/Web/CSS/grid

--- frontend/src/components/App.vue
+++ frontend/src/components/App.vue
@@ -1,6 +1,27 @@
 <template>
-	<div>
-		<router-view/>
+	<div id="application">
+
+		<nav id="primary">
+			<ul>
+				<li class="active">primary 1</li>
+				<li>primary 2</li>
+				<li>primary 3</li>
+			</ul>
+		</nav>
+
+		<nav id="secondary">
+			<ul>
+				<li>secondary 1</li>
+				<li>secondary 2</li>
+				<li>secondary 3</li>
+			</ul>
+		</nav>
+
+		<div id="router-view-container">
+			<div id="router-view">
+				<router-view/>
+			</div>
+		</div>
 	</div>
 </template>
 
