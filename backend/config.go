d42ed3845f500af60ff34d9b0c32e715132e5871
Peter Yates <peter.yates@graphia.co.uk>
2017-06-01 15:45:24 +0100

    Move RSA key paths to the config file

--- backend/config.go
+++ backend/config.go
@@ -19,6 +19,8 @@ type Config struct {
 	Static         string
 	HugoConfigFile string `yaml:"hugo_config_file"`
 	HugoBin        string `yaml:"hugo_bin"`
+	PrivateKeyPath string `yaml:"private_key_path"`
+	PublicKeyPath  string `yaml:"public_key_path"`
 }
 
 func loadConfig(path *string) (Config, error) {
