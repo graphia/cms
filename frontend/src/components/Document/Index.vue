c69b35b961b90c0210a5ed7ac382c0989630232a
Peter Yates <peter.yates@graphia.co.uk>
2017-09-18 13:08:29 +0100

    Make the document index a little prettier
    Replace the plain list with cards that contain descriptions and proper
    titles

--- frontend/src/components/Document/Index.vue
+++ frontend/src/components/Document/Index.vue
@@ -1,25 +1,54 @@
 <template>
 	<div id="document-index">
-		<h2 v-if="activeDirectory.title">
-			{{ activeDirectory.title }}
-		</h2>
-		<h2 v-else>
-			{{ directory | capitalize }}
-		</h2>
 
-		<p>{{ activeDirectory.description }}</p>
+		<div class="row document-info">
+			<div class="col-md-12">
 
-		<ul>
-			<li v-for="document in documents">
-				<router-link :to="{name: 'document_show', params: {filename: document.filename}}">
-					{{ document.absolutePath }}
+				<!-- document index header -->
+				<h2 v-if="activeDirectory.title">
+					{{ activeDirectory.title }}
+				</h2>
+				<h2 v-else>
+					{{ directory | capitalize }}
+				</h2>
+				<!-- /document index header -->
+
+				<p>{{ activeDirectory.description }}</p>
+
+			</div>
+		</div>
+
+
+		<div class="row document-list card-deck">
+
+			<div class="card col col-md-6 col-lg-3 p-3 m-1" v-for="(document, i) in documents" :key="i">
+
+					<div class="card-body">
+
+						<h4 class="card-title">
+							{{ document.title }}
+						</h4>
+
+						<p>{{ document.synopsis }}</p>
+
+						<router-link :to="{name: 'document_show', params: {filename: document.filename}}">
+							{{ document.absolutePath }}
+						</router-link>
+					</div>
+
+			</div>
+
+		</div>
+
+		<div class="row document-buttons">
+
+			<div class="col-12">
+				<router-link :to="{name: 'document_new', params: {directory: this.$route.params.directory}}" class="btn btn-primary">
+					New
 				</router-link>
-			</li>
-		</ul>
+			</div>
 
-		<router-link :to="{name: 'document_new', params: {directory: this.$route.params.directory}}" class="btn btn-primary">
-			New
-		</router-link>
+		</div>
 	</div>
 </template>
 
