c2eba3ccab9e2431c5a3158994a6d061abe7f91f
Peter Yates <peter.yates@graphia.co.uk>
2018-03-10 16:19:10 +0000

    Adjust tests now user creation is protected

--- backend/admin_handlers_test.go
+++ backend/admin_handlers_test.go
@@ -64,20 +64,12 @@ func TestAdminMiddleware(t *testing.T) {
 
 			client := &http.Client{}
 
-			// create a token and set it in the db
-			token, _ := newToken(u)
-			tokenString, _ := newTokenString(token)
-			u.setToken(tokenString)
-
 			payload, _ := json.Marshal(tt.user)
 			buff := bytes.NewBuffer(payload)
 
 			req, _ := http.NewRequest("PATCH", target, buff)
 
-			req.Header.Add(
-				"Authorization",
-				fmt.Sprintf("Bearer %s", tokenString),
-			)
+			req = authorizeRequest(u, req)
 
 			resp, _ := client.Do(req)
 
@@ -96,5 +88,108 @@ func TestAdminMiddleware(t *testing.T) {
 
 		})
 	}
+
+}
+
+func authorizeRequest(user User, req *http.Request) *http.Request {
+
+	// create a token and set it in the db
+	token, _ := newToken(user)
+	tokenString, _ := newTokenString(token)
+	user.setToken(tokenString)
+
+	// and add the auth header
+	req.Header.Add(
+		"Authorization",
+		fmt.Sprintf("Bearer %s", tokenString),
+	)
+
+	return req
+}
+
+func TestAPICreateUser(t *testing.T) {
+
+	// initial setup
+	server := setupMiddlewareAdminTestServer()
+	setupTestKeys()
+
+	// clear db
+	db.Drop("User")
+
+	// create an admin
+	createUser(ck)
+	admin, _ := getUserByUsername(ck.Username)
+
+	target := fmt.Sprintf("%s/%s", server.URL, "api/admin/users")
+
+	payload, _ := json.Marshal(mh)
+	buff := bytes.NewBuffer(payload)
+
+	req, _ := http.NewRequest("POST", target, buff)
+
+	req = authorizeRequest(admin, req)
+
+	client := &http.Client{}
+	Debug.Println(client)
+	resp, _ := client.Do(req)
+
+	// should get a 201 back if everything's successful
+	assert.Equal(t, 201, resp.StatusCode)
+
+	// make sure new user has been properly created
+	user, _ := getUserByUsername(mh.Username)
+	assert.Equal(t, mh.Username, user.Username)
+	assert.Equal(t, mh.Name, user.Name)
+	assert.Equal(t, mh.Email, user.Email)
+	assert.True(t, user.Active)
+
+	// ensure passwords don't match (i.e. encrypted)
+	assert.NotEqual(t, mh.Password, user.Password)
+
+}
+
+func TestAPICreateUserWithErrors(t *testing.T) {
+
+	// initial setup
+	server := setupMiddlewareAdminTestServer()
+	setupTestKeys()
+
+	// clear db
+	db.Drop("User")
+
+	// create an admin
+	createUser(ck)
+	admin, _ := getUserByUsername(ck.Username)
+
+	var target = fmt.Sprintf("%s/%s", server.URL, "api/admin/users")
+
+	invalid := User{
+		//Username: "misshoover",
+		Email:    "e.hoover@springfield.k12.us",
+		Name:     "Elizabeth Hoover",
+		Password: "SuperSecret123",
+	}
+
+	payload, _ := json.Marshal(invalid)
+
+	buff := bytes.NewBuffer(payload)
+
+	client := &http.Client{}
+
+	req, _ := http.NewRequest("POST", target, buff)
+	req = authorizeRequest(admin, req)
+
+	resp, _ := client.Do(req)
+
+	var receiver map[string]string
+
+	json.NewDecoder(resp.Body).Decode(&receiver)
+
+	// API should return the right response
+	assert.Contains(t, receiver["Username"], "is a required field")
+
+	// Should still equal 1 as our second attempt failed
+	au, _ := allUsers()
+	assert.Equal(t, 1, len(au))
 
 }
